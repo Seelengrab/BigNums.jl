61e74cd8b2e15c75c7d1d21877e44ef37e6e7f74
Sukera <Seelengrab@users.noreply.github.com>
2021-09-07 18:04:57 +0200

    Update PropCheck tests, add bitwise properties

--- test/properties.jl
+++ test/properties.jl
@@ -1,5 +1,42 @@
+using BigNums: BITS, normalize!, set_one!, set_zero!, bitneg!
 
 ### Properties ###
+
+###################
+### Operational ###
+###################
+
+begin
+    function checkOne()
+        a = one(ArbUInt)
+        a.data == [one(ArbDigit)]
+    end
+
+    function checkZero()
+        a = zero(ArbUInt)
+        isempty(a.data)
+    end
+
+    function isnormalized(a::ArbUInt)
+        findlast(iszero, a.data) != lastindex(a.data)
+    end
+
+    function constructNormalized(v::Vector{ArbDigit})
+        isnormalized(ArbUInt(v))
+    end
+
+    function constructNormalized(v::Union{ArbDigit, DoubleArbDigit})
+        isnormalized(ArbUInt(v))
+    end
+
+    function setOne(a)
+        set_one!(deepcopy(a)) == one(a)
+    end
+
+    function setZero(a)
+        set_zero!(deepcopy(a)) == zero(a)
+    end
+end
 
 ##################
 ### Arithmetic ###
@@ -61,32 +98,70 @@ function commutative(a, b, op)
 end
 
 begin ## Shifts ##
+    function leftShiftTrailLeadCorrect(a, sh)
+        b = a << sh
+        iszero(a) && return trailing_zeros(a) == trailing_zeros(b) == 0
+        
+        trail_correct = trailing_zeros(a) + sh == trailing_zeros(b)
+
+        sh %= BITS # => sh < BITS (the part we care about)
+
+        lead_a = leading_zeros(a)
+        lead_b = leading_zeros(b)
+        lead_correct = begin
+            if iszero(sh) # did we just shift the top most word up a number of digits?
+                lead_a == lead_b
+            elseif sh > lead_a # ok, only a part word overall, but did we overflow?
+                lead_b == BITS - (sh - lead_a)
+            elseif sh < lead_a # what about without overflow?
+                lead_b == lead_a - sh
+            else # and what if we shift exactly up to the boundary?
+                iszero(lead_b)
+            end
+        end
+
+        return trail_correct && lead_correct
+    end
+
+    function leftShiftNoDataLoss(a, sh)
+        b = a << sh
+        count_ones(b) == count_ones(a)
+    end
+
+    function additionOfShifts(a, (sh1, sh2)::Tuple{T,T}) where T <: Unsigned
+        T1 = widen(T)
+        ((a << sh1) << sh2) == (a << (T1(sh1) + T1(sh2)))
+    end
+
     function shiftAntiInverse(a, sh)
         ((a << sh) >> sh) == a
     end
 
-    function shiftSame(a::ArbUInt, sh)
+    function shiftValueSubtraction(a, (sh1,sh2)::Tuple{T,T}) where T <: Unsigned
+        # property assumes sh1 <= sh2
+        b = (a << sh2) >> sh1
+        c = a << (sh2 - sh1)
+        b == c
+    end
+
+    function rightShiftSame(a::ArbUInt, sh)
         (a >>> sh) == (a >> sh)
     end
 
     function shiftIdentity(a)
-        (a >> 0) == a && (a << 0) == a
+        (a >> 0x0) == a && (a << 0x0) == a
     end
 end
 
 begin ## Negation ##
     function inverse(a)
-        b = deepcopy(a)
-        if iszero(Base.leading_zeros(last(a.data)))
-            push!(b.data ,zero(ArbDigit))
-        end
-        b = ~(~b)
+        a == ~(~a)
     end
 end
 
 begin ## And ##
     function zeroing_and(a)
-        (a & 0) == zero(a) && (0 & a) == zero(a)
+        (a & zero(a)) == zero(a) && (zero(a) & a) == zero(a)
     end
 
     function identity_and(a)
@@ -112,20 +187,23 @@ end
 
 begin ## Nor ##
     function chaining_nor(a,b)
-        nor(a,b) == ~(a | b)
+        nor(a,b) == normalize!(~(a | b))
     end
 end
 
 begin ## Nand ##
     function chaining_nand(a,b)
-        nand(a,b) == ~(a & b)
+        nand(a,b) == normalize!(~(a & b))
     end
 end
 
 begin ## Combined Properties ##
     function lossyShift(a, sh)
         b = (a >> sh) << sh
-        c = ~(one(ArbUInt) << (sh+1))
-        (a ⊻ ~(a & c)) == b
+        # mask out the parts we shifted out in b
+        mask = ArbUInt(unsigned(-1) % ArbDigit) << sh
+        pop!(mask.data)
+        bitneg!(mask)
+        (b ⊻ (a & mask)) == a
     end
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,14 @@
-module BigNumTests
-
 using BigNums
-using BigNums: ArbDigit
+using BigNums: ArbDigit, DoubleArbDigit
 using Test
 using PropCheck
-using Combinatorics: permutations
+using Random
 
 ### Function Defs
 
-genArbUInt(i) = Integrated(Generator{ArbUInt}((rng) -> ArbUInt(root(generate(rng, PropCheck.vector(i, Integrated(Generator(ArbDigit))))))))
-PropCheck.generate(::Type{ArbUInt}) = ArbUInt(generate(UInt, (rand(UInt) % 20) + 1))
-PropCheck.shrink(t::ArbUInt) = Iterators.map(ArbUInt, shrink(t.data))
+genArbUInt(i=5) = Integrated(Generator{ArbUInt}(rng -> ArbUInt(root(generate(rng, PropCheck.vector(i, igen(ArbDigit)))))))
+PropCheck.generate(rng, ::Type{ArbUInt}) = genArbUInt(rng)
+PropCheck.shrink(t::ArbUInt) = PropCheck.iunique(Iterators.map(ArbUInt, shrink(t.data)))
 
 # PropCheck.specials(::Type{ArbUInt}) = ArbUInt[
 #     ArbUInt.(must_tests)...,
@@ -23,12 +21,24 @@ include("properties.jl")
 ### Tests ###
 
 @testset "Tests" begin
+@testset "Construction" begin
+    @test check(isnormalized, genArbUInt())
+    @test checkOne()
+    @test checkZero()
+    @test check(setOne, genArbUInt())
+    @test check(setZero, genArbUInt())
+    @testset "constructNormalized($T)" for (gen,T) in ((PropCheck.vector(5, igen(ArbDigit)),Vector{ArbDigit}),
+                                                        (igen(ArbDigit),ArbDigit),
+                                                        (igen(DoubleArbDigit),DoubleArbDigit))
+        @test check(constructNormalized, gen)
+    end
+end
 @testset "Addition" begin
     @testset "$f" for (n,f) in ((2,commutative_add),
                                 (3,associative_add),
                                 (1,identity_add))
-        gen = PropCheck.tuple(n, genArbUInt(20))
-        @test check(gen, Base.splat(f))
+        gen = PropCheck.tuple(n, genArbUInt())
+        @test check(Base.splat(f), gen)
     end
 end
 @testset "Multiplication" begin
@@ -36,78 +46,95 @@ end
                             (3,associative_mul),
                             (1,identity_mul),
                             (1,zero_mul))
-        gen = PropCheck.tuple(n, genArbUInt(20))
-        @test check(gen, Base.splat(f))
+        gen = PropCheck.tuple(n, genArbUInt())
+        @test check(Base.splat(f), gen)
     end
 end
 @testset "Bitwise" begin
     @testset "Shifts" begin
-        @testset "$shiftAntiInverse" begin
-            # gen = 
-            # @test check(gen, Base.splat(shiftAntiInverse))
-            @test_broken false
+        @testset "$leftShiftNoDataLoss" begin
+            gen = interleave(genArbUInt(), igen(UInt8))
+            @test check(Base.splat(leftShiftNoDataLoss), gen)
         end
-        @testset "$shiftSame" begin
-            @test_broken false
+        @testset "$leftShiftTrailLeadCorrect" begin
+            gen = interleave(genArbUInt(), igen(UInt8)) # shifting more would be irresponsible
+            @test check(Base.splat(leftShiftTrailLeadCorrect), gen)
+        end
+        @testset "$additionOfShifts" begin
+            gen = interleave(genArbUInt(), igen(Tuple{UInt8, UInt8}))
+            @test check(Base.splat(additionOfShifts), gen) 
+        end
+        @testset "$shiftAntiInverse" begin
+            gen = interleave(genArbUInt(), igen(UInt8)) # shifting more would be irresponsible
+            @test check(Base.splat(shiftAntiInverse), gen)
+        end
+        @testset "$shiftValueSubtraction" begin
+            shiftGen = filter(issorted, igen(Tuple{UInt8, UInt8}))
+            gen = interleave(genArbUInt(), shiftGen)
+            @test check(Base.splat(shiftValueSubtraction), gen)
+        end
+        @testset "$rightShiftSame" begin
+            gen = interleave(genArbUInt(), igen(UInt8)) # shifting more would be irresponsible
+            @test check(Base.splat(rightShiftSame), gen) broken=true
         end
         @testset "$shiftIdentity" begin
-            gen = Integrated(genArbUInt(20))
-            @test check(gen, shiftIdentity)
+            gen = genArbUInt()
+            @test check(shiftIdentity, gen)
         end
         @testset "$lossyShift" begin
-            @test_broken false
+            gen = interleave(genArbUInt(), igen(UInt8))
+            @test check(Base.splat(lossyShift), gen)
         end
     end
     @testset "negation" begin
         @testset "$inverse" begin
-            gen = Integrated(genArbUInt(20))
-            @test check(gen, inverse)
+            gen = genArbUInt()
+            @test check(inverse, gen)
         end
     end
     @testset "and" begin
         @testset "$f" for (n,f) in ((2,commutative(&)),
                                     (1,zeroing_and),
                                     (1,identity_and))
-            gen = PropCheck.tuple(n, genArbUInt(20))
-            @test check(gen, Base.splat(f))
+            gen = PropCheck.tuple(n, genArbUInt())
+            @test check(Base.splat(f), gen)
         end
     end
     @testset "or" begin
         @testset "$f" for (n,f) in ((2,identity_or),
                                     (2,commutative(|)))
-            gen = PropCheck.tuple(2, genArbUInt(20))
-            @test check(gen, Base.splat(f))
+            gen = PropCheck.tuple(n, genArbUInt())
+            @test check(Base.splat(f), gen)
         end
     end
     @testset "xor" begin
         @testset "$zeroing_xor" begin
-            gen = genArbUInt(20)
-            @test check(gen, zeroing_xor)
+            gen = genArbUInt()
+            @test check(zeroing_xor, gen)
         end
         @testset "$identity_xor" begin
-            gen = genArbUInt(20)
-            @test check(gen, identity_xor)
+            gen = genArbUInt()
+            @test check(identity_xor, gen)
         end
         @testset "$commutative" begin
-            gen = PropCheck.tuple(2, genArbUInt(20))
-            @test check(gen, Base.splat(commutative(⊻)))
+            gen = PropCheck.tuple(2, genArbUInt())
+            @test check(Base.splat(commutative(⊻)), gen)
         end
     end
     @testset "nor" begin
         @testset "$f" for (n,f) in ((2,chaining_nor),
                                     (2,commutative(⊽)))
-            gen = PropCheck.tuple(2, genArbUInt(20))
-            @test check(gen, Base.splat(f))
+            gen = PropCheck.tuple(n, genArbUInt())
+            @test check(Base.splat(f), gen)
         end
     end
     @testset "nand" begin
         @testset "$f" for (n,f) in ((2,chaining_nand),
                                     (2,commutative(⊼)))
-            gen = PropCheck.tuple(2, genArbUInt(20))
-            @test check(gen, Base.splat(f))
+            gen = PropCheck.tuple(n, genArbUInt())
+            @test check(Base.splat(f), gen)
         end
     end
 end
 
-end
 end
